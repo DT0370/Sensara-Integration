faa67297987cea45edff152ea0f8e3eefeac95c0
Vackyyyy <74583294+Vackyyyy@users.noreply.github.com>
2024-12-12 18:47:08 +0530

    Update sales_invoice.py

--- sensara_integration/sensara_integration/customization/delivery_note/sales_invoice.py
+++ sensara_integration/sensara_integration/customization/delivery_note/sales_invoice.py
@@ -70,7 +70,7 @@ def create_subscription_payload(doc,action):
             start_timestamp = datetime.datetime.strptime(doc.custom_start_timestamp_for_the_plan, "%Y-%m-%d").isoformat()
             end_timestamp = datetime.datetime.strptime(doc.custom_end_timestamp_for_the_plan, "%Y-%m-%d").isoformat()
     delivery_note_number = frappe.get_doc("Delivery Note", doc.custom_parent_delivery_note)
-	body = {
+body = {
             "action": action,
             "phone_number": delivery_note_number.contact_mobile,
             "country_code": doc.custom_country_code,
