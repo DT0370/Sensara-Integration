59fa5c642b1ffe465b5b929f7822f7e634db8af4
gajanan <gajanan.pathak@streamboxmedia.com>
2025-01-03 15:02:03 +0530

    adding the subscription_id updated code

--- sensara_integration/sensara_integration/doctype/dorplay_subscription_activation/dorplay_subscription_activation.py
+++ sensara_integration/sensara_integration/doctype/dorplay_subscription_activation/dorplay_subscription_activation.py
@@ -4,95 +4,112 @@
 import frappe
 from frappe.model.document import Document
 import datetime
-import re
-import requests, json
-from requests.exceptions import HTTPError 
+import json
+import requests
+from requests.exceptions import HTTPError
 
 
 class DorplaySubscriptionActivation(Document):
-	def after_insert(self):
-		body, webhook_log = self.create_dorplay_subscription_payload()
-		post_subscription_payload(body, webhook_log)
+    def after_insert(self):
+        body, webhook_log = self.create_dorplay_subscription_payload()
+        post_subscription_payload(body, webhook_log)
 
-	def create_dorplay_subscription_payload(self):
-		sensara_settings = frappe.get_doc('Sensara Integration Settings')
-		headers = {
-			'content-type':'application/json',
-			sensara_settings.dorplay_api_key: sensara_settings.dorplay_api_secret
-		}
+    def create_dorplay_subscription_payload(self):
+        # Fetch Sensara Integration Settings
+        sensara_settings = frappe.get_doc('Sensara Integration Settings')
+        headers = {
+            'content-type': 'application/json',
+            sensara_settings.dorplay_api_key: sensara_settings.dorplay_api_secret
+        }
 
-		plan = {}
-		entitlements = []
-		tv_device_serial_number = ""
+        # Initialize variables
+        plan = {}
+        entitlements = []
 
-		product_bundle_list = frappe.get_all("Product Bundle",["new_item_code"],pluck="new_item_code")
-		plan_name = frappe.get_value("Item",self.plan,['item_name'])
-		plan.update({"plan_id":self.plan, "id":plan_name})
-		pdt_bundle = frappe.get_doc("Product Bundle",self.plan)
-		for item in pdt_bundle.items:
-			item_name = frappe.get_value("Item",item.item_code,['item_name'])
-			entitlements.append({
-				"id": plan_name,
-				"entitlement_id": item.item_code,
-				"entitlement_type": "ott",
-				"entitlement_title": item_name
-			})
+        # Get product bundle details
+        product_bundle_list = frappe.get_all("Product Bundle", ["new_item_code"], pluck="new_item_code")
+        plan_name = frappe.get_value("Item", self.plan, ['item_name'])
+        plan.update({"plan_id": self.plan, "id": plan_name})
 
-		plan.update({"entitlements":entitlements})
+        # Add entitlements for the plan
+        pdt_bundle = frappe.get_doc("Product Bundle", self.plan)
+        for item in pdt_bundle.items:
+            item_name = frappe.get_value("Item", item.item_code, ['item_name'])
+            entitlements.append({
+                "id": plan_name,
+                "entitlement_id": item.item_code,
+                "entitlement_type": "ott",
+                "entitlement_title": item_name
+            })
 
-		if isinstance(self.start_timestamp_for_the_plan, datetime.datetime):
-			start_timestamp = self.start_timestamp_for_the_plan.isoformat()
-			end_timestamp = self.end_timestamp_for_the_plan.isoformat()
-		if isinstance(self.start_timestamp_for_the_plan, str):
-			try:
-				start_timestamp = datetime.datetime.strptime(self.start_timestamp_for_the_plan, "%Y-%m-%d %H:%M:%S").isoformat()
-				end_timestamp = datetime.datetime.strptime(self.end_timestamp_for_the_plan, "%Y-%m-%d %H:%M:%S").isoformat()
-			except Exception as e:
-				start_timestamp = datetime.datetime.strptime(self.start_timestamp_for_the_plan, "%Y-%m-%d").isoformat()
-				end_timestamp = datetime.datetime.strptime(self.end_timestamp_for_the_plan, "%Y-%m-%d").isoformat()
+        plan.update({"entitlements": entitlements})
 
-		body = {
-			"action": "SUBSCRIPTION_ACTIVATION",
-			"phone_number": self.phone_number,
-			"country_code": self.country_code,
-			"customer_id": self.customer,
-			"start_timestamp": str(start_timestamp) + "Z", 
-			"end_timestamp": str(end_timestamp) + "Z",
-			"subscription_id": self.subscription_id if self.subscription_id else preview_subscription_id,
-			"subscription_type": "Non-TV",
-			"tv_device_serial_number": None,
-			"tv_model": None,
-			"activation_status": None
-			} 
-		body.update({"plan": plan})
+        # Convert timestamps
+        if isinstance(self.start_timestamp_for_the_plan, datetime.datetime):
+            start_timestamp = self.start_timestamp_for_the_plan.isoformat()
+            end_timestamp = self.end_timestamp_for_the_plan.isoformat()
+        elif isinstance(self.start_timestamp_for_the_plan, str):
+            try:
+                start_timestamp = datetime.datetime.strptime(self.start_timestamp_for_the_plan, "%Y-%m-%d %H:%M:%S").isoformat()
+                end_timestamp = datetime.datetime.strptime(self.end_timestamp_for_the_plan, "%Y-%m-%d %H:%M:%S").isoformat()
+            except Exception:
+                start_timestamp = datetime.datetime.strptime(self.start_timestamp_for_the_plan, "%Y-%m-%d").isoformat()
+                end_timestamp = datetime.datetime.strptime(self.end_timestamp_for_the_plan, "%Y-%m-%d").isoformat()
 
-		# Make the POST request
-		webhook_log = frappe.new_doc("Sensara Integration Request Log")
-		webhook_log.request_for = "Dorplay Subscription Activation"
-		webhook_log.reference_document = self.name
-		webhook_log.headers = str(headers)
-		webhook_log.data = str(json.dumps(body))
-		webhook_log.user = self.modified_by
-		webhook_log.url = sensara_settings.dorplay_base_url
+        # Extract preview_subscription_id
+        preview_subscription_id = getattr(self, "preview_subscription_id", None)
 
-		return body,webhook_log
+        # Construct request body
+        body = {
+            "action": "SUBSCRIPTION_ACTIVATION",
+            "phone_number": self.phone_number,
+            "country_code": self.country_code,
+            "customer_id": self.customer,
+            "start_timestamp": str(start_timestamp) + "Z",
+            "end_timestamp": str(end_timestamp) + "Z",
+            "subscription_id": self.subscription_id if self.subscription_id else preview_subscription_id,
+            "subscription_type": "Non-TV",
+            "tv_device_serial_number": None,
+            "tv_model": None,
+            "activation_status": None
+        }
+        body.update({"plan": plan})
+
+        # Create webhook log
+        webhook_log = frappe.new_doc("Sensara Integration Request Log")
+        webhook_log.request_for = "Dorplay Subscription Activation"
+        webhook_log.reference_document = self.name
+        webhook_log.headers = str(headers)
+        webhook_log.data = json.dumps(body)
+        webhook_log.user = self.modified_by
+        webhook_log.url = sensara_settings.dorplay_base_url
+
+        return body, webhook_log
+
 
 def post_subscription_payload(body, webhook_log):
-	sensara_settings = frappe.get_doc('Sensara Integration Settings')
-	headers = {
-		'content-type':'application/json',
-		sensara_settings.dorplay_api_key: sensara_settings.dorplay_api_secret
-	}
-	try:
-		response = requests.post(sensara_settings.dorplay_base_url,headers=headers,data=json.dumps(body))
-		webhook_log.response = response
-		try:
-			webhook_log.message = str(response.json())
-		except Exception as e:
-			print(e)
+    # Fetch Sensara Integration Settings
+    sensara_settings = frappe.get_doc('Sensara Integration Settings')
+    headers = {
+        'content-type': 'application/json',
+        sensara_settings.dorplay_api_key: sensara_settings.dorplay_api_secret
+    }
 
-	except HTTPError as http_err:
-		webhook_log.error = http_err
-		frappe.throw(_("HTTP Error {0}".format(http_err)))
+    try:
+        # Make the POST request
+        response = requests.post(sensara_settings.dorplay_base_url, headers=headers, data=json.dumps(body))
+        webhook_log.response = response.text
 
-	webhook_log.insert(ignore_permissions=True)
+        # Add response message
+        try:
+            webhook_log.message = str(response.json())
+        except Exception as e:
+            print(e)
+
+    except HTTPError as http_err:
+        webhook_log.error = str(http_err)
+        frappe.throw(_("HTTP Error {0}".format(http_err)))
+
+    # Save webhook log
+    webhook_log.insert(ignore_permissions=True)
+
--- sensara_integration/sensara_integration/doctype/subscription_deactivation/subscription_deactivation.py
+++ sensara_integration/sensara_integration/doctype/subscription_deactivation/subscription_deactivation.py
@@ -6,44 +6,62 @@
 from frappe.model.document import Document
 import frappe
 from frappe import _
-import re
-import requests, json
-from requests.exceptions import HTTPError 
-import frappe
+import requests
+import json
+from requests.exceptions import HTTPError, RequestException
 import datetime
 
 
 class SubscriptionDeactivation(Document):
-	
-	def on_submit(self):
-		sensara_settings = frappe.get_doc('Sensara Integration Settings')
+    def on_submit(self):
+        sensara_settings = frappe.get_doc('Sensara Integration Settings')
 
-		headers = {
-				'content-type':'application/json',
-            	sensara_settings.api_key: sensara_settings.api_secret
-				
-			}
-		body = {
-			"action": self.action,
-			"phone_number": self.phone_number,
-			"country_code": "+91",
-			"subscription_id": self.subscription_id if self.subscription_id else preview_subscription_id,
-		}
+        headers = {
+            'content-type': 'application/json',
+            sensara_settings.api_key: sensara_settings.api_secret
+        }
 
-		webhook_log = frappe.new_doc("Sensara Integration Request Log")
-		webhook_log.request_for = self.action
-		webhook_log.reference_document = self.name
-		webhook_log.headers = str(headers)
-		webhook_log.data = str(json.dumps(body))
-		webhook_log.user = self.modified_by
-		webhook_log.url = sensara_settings.base_url
+        # Safely handle preview_subscription_id
+        preview_subscription_id = getattr(self, "preview_subscription_id", None)
 
-		try:
-			response = requests.post(sensara_settings.base_url,headers=headers,data=json.dumps(body))
-			webhook_log.response = response
-			webhook_log.message = str(response.json())
+        body = {
+            "action": self.action,
+            "phone_number": self.phone_number,
+            "country_code": "+91",
+            "subscription_id": self.subscription_id if self.subscription_id else preview_subscription_id,
+        }
 
-		except HTTPError as http_err:
-			webhook_log.error = http_err
-			# frappe.throw(_("HTTP Error {0}".format(http_err)))
-		webhook_log.insert()
+        # Create a webhook log
+        webhook_log = frappe.new_doc("Sensara Integration Request Log")
+        webhook_log.request_for = self.action
+        webhook_log.reference_document = self.name
+        webhook_log.headers = json.dumps(headers)
+        webhook_log.data = json.dumps(body)
+        webhook_log.user = self.modified_by
+        webhook_log.url = sensara_settings.base_url
+
+        try:
+            # Make the POST request
+            response = requests.post(sensara_settings.base_url, headers=headers, data=json.dumps(body))
+
+            # Set response and message
+            webhook_log.response = response.text
+            try:
+                webhook_log.message = str(response.json())
+            except ValueError:
+                webhook_log.message = "Unable to parse response as JSON"
+
+        except HTTPError as http_err:
+            webhook_log.error = f"HTTP Error: {http_err}"
+            frappe.log_error(message=str(http_err), title="HTTP Error in Subscription Deactivation")
+        except RequestException as req_err:
+            webhook_log.error = f"Request Error: {req_err}"
+            frappe.log_error(message=str(req_err), title="Request Error in Subscription Deactivation")
+        except Exception as ex:
+            webhook_log.error = f"Unexpected Error: {ex}"
+            frappe.log_error(message=str(ex), title="Unexpected Error in Subscription Deactivation")
+        finally:
+            # Ensure the webhook log is saved
+            webhook_log.insert(ignore_permissions=True)
+            frappe.db.commit()
+
